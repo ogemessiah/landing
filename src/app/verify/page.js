c6b73af3876e9107685fef2659f1684d56c941d9
Onwuemerie Ogechi <onwuemerieogechi@Onwuemeries-Mac-mini.local>
2026-08-06 14:07:53 +0100

    fixing verification

---
+++ src/app/verify/page.js
@@ -0,0 +1,22 @@
+export default function VerifyPage() {
+  return (
+    <div
+      style={{
+        maxWidth: 600,
+        margin: "100px auto",
+        textAlign: "center",
+        fontFamily: "Arial"
+      }}
+    >
+      <h1 style={{ color: "#04B559" }}>
+        TunnelMouth
+      </h1>
+
+      <h2>Email Verification</h2>
+
+      <p>
+        Your email has been verified successfully.
+      </p>
+    </div>
+  );
+}
